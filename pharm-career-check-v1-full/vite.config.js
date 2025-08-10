a6b99e1691871b8cc192272deea4b03542cb48f3
yuki0120yuki <uriuri0120@yahoo.co.jp>
2025-08-10 10:06:16 +0900

    Update vite.config.js

--- pharm-career-check-v1-full/vite.config.js
+++ pharm-career-check-v1-full/vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Vercel の Output Directory はこれに合わせます
-  }
-})
+});
