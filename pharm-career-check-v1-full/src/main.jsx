a0a71dda7cd6f560f099ec7a94321904c5f51e1b
yuki0120yuki <uriuri0120@yahoo.co.jp>
2025-08-10 10:20:31 +0900

    Update main.jsx

--- pharm-career-check-v1-full/src/main.jsx
+++ pharm-career-check-v1-full/src/main.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./styles.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
