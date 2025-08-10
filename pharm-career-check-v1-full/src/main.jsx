150dc99b06f387269b7595c418a215afdd26ef0e
yuki0120yuki <uriuri0120@yahoo.co.jp>
2025-08-10 09:32:44 +0900

    Update main.jsx

--- pharm-career-check-v1-full/src/main.jsx
+++ pharm-career-check-v1-full/src/main.jsx
@@ -3,4 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>)
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
